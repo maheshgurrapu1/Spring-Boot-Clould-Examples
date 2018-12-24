5feaf68ea4da072f9892c0980771557c8a6f085f
mgurrapu <mgurrapu@mgurrapu-lp.pkrmsezdc.com>
2018-12-24 18:25:07 +0530

    Test

--- restful-web-services/src/main/java/com/mahesh/rest/webservices/bean/User.java
+++ restful-web-services/src/main/java/com/mahesh/rest/webservices/bean/User.java
@@ -2,12 +2,17 @@ package com.mahesh.rest.webservices.bean;
 
 import java.util.Date;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 public class User {
 
 	private Integer id;
 
+	@Size(min = 2)
 	private String name;
 
+	@Past
 	private Date birthdate;
 
 	public Integer getId() {
--- restful-web-services/src/main/java/com/mahesh/rest/webservices/controller/UserResource.java
+++ restful-web-services/src/main/java/com/mahesh/rest/webservices/controller/UserResource.java
@@ -3,6 +3,8 @@ package com.mahesh.rest.webservices.controller;
 import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -41,7 +43,7 @@ public class UserResource {
 	
 	
 	@PostMapping("/users")
-	public ResponseEntity<Object> createUser(@RequestBody User user) {
+	public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
 		User savedUser = userDao.save(user);
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
 				.toUri();
